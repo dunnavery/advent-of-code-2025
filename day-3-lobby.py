297df79453a58abaf21045cbbd7459832214441b
Avery Dunn <avery.dunn@nike.com>
2025-12-03 11:06:42 -0800

    Day 3: Lobby puzzle complete

---
+++ day-3-lobby.py
@@ -0,0 +1,53 @@
+
+file_path = './puzzle-input.txt'
+
+try:
+    # Part 1: finding largest joltage of numbers per battery
+    with open(file_path, 'r') as file:
+        total_joltage = 0
+        for line in file:
+            max_joltage = 0
+            for i in range(len(line)):
+                for j in range(i+1, len(line)):
+                    joltage = f"{line[i]}{line[j]}"
+                    joltage = int(joltage)
+                    if joltage > max_joltage:
+                        max_joltage = joltage
+            total_joltage += max_joltage
+        print(f"Total joltage = {total_joltage}")
+except FileNotFoundError:
+    print(f"Error: the file path was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+try:
+    # Part 2: finding largest joltage of numbers per battery
+    with open(file_path, 'r') as file:
+        total_joltage = 0
+        for line in file:
+            line = line.strip()
+            remaining_list = line
+            joltage = []
+            for i in reversed(range(12)):
+                if i != 0:
+                    element = max(remaining_list[:-i])
+                    joltage.append(element)
+                    element_idx = remaining_list.index(f'{element}')
+                    remaining_list = remaining_list[element_idx+1:]
+                elif len(remaining_list) > 0:
+                    element = max(remaining_list)
+                    joltage.append(element)
+
+            joltage = int(''.join(joltage))
+            print(f"Joltage per battery {joltage}")
+            total_joltage += joltage
+
+        print(f"Total joltage = {total_joltage}")
+
+except FileNotFoundError:
+    print(f"Error: the file path was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
