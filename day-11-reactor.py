350535b89c24a337383bd00c86ec9bf04dfb5ebe
Avery Dunn <avery.dunn@nike.com>
2025-12-12 09:03:25 -0800

    Day 11 Part 1 Done

--- day-11-reactor.py
+++ day-11-reactor.py
@@ -14,19 +14,19 @@ with open(file_path, 'r') as file:
 
 def traverse_devices(device, devices):
     global out_count
-#     global fft_count
-#     global dac_count
-#     global fft_and_dac_count
+    global fft_count
+    global dac_count
+    global fft_and_dac_count
     next_devices = devices[device]
     print(next_devices)
-#     if 'fft' in next_devices:
-#         fft_count += 1
-#     if 'dac' in next_devices:
-#         dac_count += 1
+    if 'fft' in next_devices:
+        fft_count += 1
+    if 'dac' in next_devices:
+        dac_count += 1
     if 'out' in next_devices:
         out_count += 1
-#         if fft_count and dac_count:
-#             fft_and_dac_count += 1
+        if fft_count and dac_count:
+            fft_and_dac_count += 1
         return
     for next_device in next_devices:
         traverse_devices(next_device, devices)
@@ -37,13 +37,14 @@ start_devices = devices['you'] # Part 1: starts at 'you' and goes to 'out'
 
 # Must also visit 'dac' and 'fft'
 out_count = 0
-# fft_and_dac_count = 0
+fft_and_dac_count = 0
 for device in start_devices:
-#     fft_count = 0
-#     dac_count = 0
+    memo = set()
+    fft_count = 0
+    dac_count = 0
     traverse_devices(device, devices)
 
-# print(f"FFT and DAC count: {fft_and_dac_count}")
+print(f"FFT and DAC count: {fft_and_dac_count}")
 print(f"Out count: {out_count}")
 
 
